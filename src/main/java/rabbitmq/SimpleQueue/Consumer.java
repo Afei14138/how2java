19231b35f84c17b1601ecb83991020ab5ca9c963
wangpf45 <wangpf45@chinaunicom.cn>
2020-03-03 22:10:33 +0800

    简单队列的生产者与消费者

---
+++ src/main/java/rabbitmq/SimpleQueue/Consumer.java
@@ -0,0 +1,32 @@
+package rabbitmq.SimpleQueue;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.DeliverCallback;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Consumer {
+
+    private static final String QUEUE_NAME = "test_simple_queue";
+
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        Connection connection = ConnectionUtil.getConnection();
+
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, false,false,false, null);
+
+        DeliverCallback deliverCallback = (consumerTag, delivery)->{
+            String message = new String(delivery.getBody());
+            System.out.println(message);
+        };
+
+        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag->{});
+
+
+    }
+}
---
+++ src/main/java/rabbitmq/SimpleQueue/Producer.java
@@ -0,0 +1,33 @@
+package rabbitmq.SimpleQueue;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Producer {
+
+    private static final String QUEUE_NAME = "test_simple_queue";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+
+        //链接MQ
+        Connection connection = ConnectionUtil.getConnection();
+
+        //创建一个通道
+        Channel channel = connection.createChannel();
+
+        //创建队列声明
+        channel.queueDeclare(QUEUE_NAME, false,false, false, null);
+
+        String msg = "Hello RabbitMQ";
+
+        channel.basicPublish("", QUEUE_NAME, null, msg.getBytes());
+
+        channel.close();
+
+        connection.close();
+
+    }
+}
